7b499a50e0e4704f98a0a3cb188f28738ca11ad6
YarelyJ <154484018+YarelyJ@users.noreply.github.com>
2023-12-28 16:55:30 -0600

    Add files via upload

---
+++ barco.java
@@ -0,0 +1,52 @@
+
+package com.mycompany.yarely_3102;
+
+public class barco {
+        public static void main(String[] args) {
+        int n=5;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++)
+                System.out.print("*");
+            for (int j=i;j<n;j++)
+                System.out.print(" ");
+            for (int j=i;j<n;j++)
+                System.out.print(" ");
+            for (int j=1;j<=i;j++)
+                System.out.print("*");
+            System.out.println();
+        }
+       for (int i=1;i>=n;i++){
+           for (int j=i;j<n;j++)
+               System.out.print("*");
+           for (int j=1;j<=i;j++)
+               System.out.print(" ");
+           for (int j=1;j<=i;j++)
+               System.out.print(" ");
+           for (int j=i;j<n;j++)
+               System.out.println("*");
+           System.out.println();
+       }
+        for (int i=1;i<n;i++){
+            for (int j=i;j<n;j++)
+                System.out.print("*");
+            for (int j=1;j<=i;j++)
+                System.out.print(" ");
+            for (int j=1;j<=i;j++)
+                System.out.print(" ");
+            for (int j=i;j<n;j++)
+                System.out.print("*");
+            System.out.println();
+        }
+       for (int i=1;i>n;i++){
+           for (int j=1;j<=i;j++)
+               System.out.print("*");
+           for (int j=i;j<n;j++)
+               System.out.print(" ");
+           for (int j=i;j<n;j++)
+               System.out.print(" ");
+           for (int j=1;j<=i;j++)
+               System.out.println("*");
+           System.out.println();
+       } 
+    }
+}
