bfd0b8ce38091015eb264b463f58ff2ab92ecf8a
YarelyJ <154484018+YarelyJ@users.noreply.github.com>
2024-01-03 19:41:05 -0600

    Add files via upload

---
+++ Multii.java
@@ -0,0 +1,41 @@
+import javax.swing.JOptionPane;
+public class Multii {
+       public static void main (String []args){
+            short mut1 [][] = new short [2][2];
+            short mut2 [][] = new short [2][2];
+          
+           short multi = 0;
+            byte x ,v = 0,b = 0,y,m=0,n=0,f,l=0,k=0;
+            for(k=0;k<=2;k++){
+            for(x=0;x<=2;x++){
+                mut1[v][b]=Short.parseShort(JOptionPane.showInputDialog("inserta numeros matriz 1"));
+             for(y=0;y<=2;y++){
+                 mut2[m][n]=Short.parseShort(JOptionPane.showInputDialog("inserta numeros matriz 2 "));
+             multi= (short) (mut1[v][b]*mut2[m][n]);
+                
+              System.out.print( multi+" ");
+            // System.out.print("\t");
+            
+            }
+            System.out.println();
+            }
+            System.out.println();
+            }
+            
+            for(k=0;k<=2;k++){
+                for(k=0;k<=8;k++){
+                    System.out.print(mut1[v][b]+" ");
+                
+                
+               
+                for(k=0;k<=8;k++){
+                 System.out.print(mut2[m][n]+" ");
+           System.out.println();
+            }
+                 System.out.println();
+                }
+                
+       }
+       }
+}
+
