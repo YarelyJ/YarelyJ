e8f1001e45aa1513592c7f4a2a9783052336c4c9
YarelyJ <154484018+YarelyJ@users.noreply.github.com>
2024-01-19 17:12:15 -0600

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+package Yarely_3102;
+import javax.swing.JOptionPane;
+public class Factorial {
+    public int factor(){
+        int factorial=1, numero;
+        numero=Integer.parseInt(JOptionPane.showInputDialog("numero"));
+        while (numero !=0){
+            factorial= (factorial*numero);
+            numero--;   
+        }
+       JOptionPane.showMessageDialog(null,"resultado es "+factorial);
+    return factorial;
+    }
+}
